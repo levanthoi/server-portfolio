39aed1c578b42c844a8907991a84fd6a6fa3d4b6
levanthoi <lethoi1506@gmail.com>
2023-09-08 00:27:07 +0700

    fix

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,27 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { IRequestBody } from '@interfaces/custom.interface';
-import { IUser } from '@interfaces/user.interface';
 import { getProjectsService } from '@services/project.service';
 
-export const getProjects = asyncHandler(
-  async (req: IRequestBody<IUser>, res: Response, next: NextFunction) => {
-    try {
-      // console.log('user', req.user);
+export const getProjects = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // console.log('user', req.user);
 
-      const projects = await getProjectsService(req.query);
+    const projects = await getProjectsService(req.query);
 
-      // const projects = await repositories.json();
+    // const projects = await repositories.json();
 
-      res.json({
-        data: projects,
-        message: 'Danh sách dự án đã được lấy thành công',
-        success: true,
-      });
-    } catch (error: any) {
-      // throw new Error(error);
-      next(error);
-    }
-  },
-);
+    res.json({
+      data: projects,
+      message: 'Danh sách dự án đã được lấy thành công',
+      success: true,
+    });
+  } catch (error: any) {
+    // throw new Error(error);
+    next(error);
+  }
+});
