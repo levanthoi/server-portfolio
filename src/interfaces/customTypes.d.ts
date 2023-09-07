a7ed35d8bc54b2909c031576c533149293d7e03a
levanthoi <lethoi1506@gmail.com>
2023-09-08 01:13:17 +0700

    fix

--- src/interfaces/customTypes.d.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from './user.interface';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: IUser; // Add the user property to the Request type
-    }
-  }
-}
--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -16,12 +16,15 @@ import {
  * @returns
  */
 const getContributors = async (owner: string, repo: string) => {
-  const response = await axios.get(`${envConfig.API_GITHUB_REPO_2}/${owner}/${repo}/contributors`, {
-    headers: {
-      Authorization: `token ${envConfig.GH_TOKEN}`,
-    },
-  });
-  if (response?.data?.length > 0) {
+  try {
+    const response = await axios.get(
+      `${envConfig.API_GITHUB_REPO_2}/${owner}/${repo}/contributors`,
+      {
+        headers: {
+          Authorization: `token ${envConfig.GH_TOKEN}`,
+        },
+      },
+    );
     const contributors: IContributors[] = response?.data?.map((contributor: IContributors) => {
       const { id, login, html_url: html, avatar_url: avt } = contributor;
       return {
@@ -31,9 +34,12 @@ const getContributors = async (owner: string, repo: string) => {
         avatar_url: avt,
       };
     });
-    return contributors;
+    return contributors || [];
+    // return [];
+  } catch (err) {
+    console.log('getContributors error', err);
+    return [];
   }
-  return [];
 };
 
 /**
@@ -99,67 +105,71 @@ const getRepoLanguages = async (owner: string, repo: string) => {
 };
 
 export const getProjectsService = async (params: IParams) => {
-  console.log(params);
+  try {
+    console.log(params);
+    const response = await axios.get(`${envConfig.API_GITHUB_REPO}`, {
+      headers: {
+        Authorization: `token ${envConfig.GH_TOKEN}`,
+      },
+      params,
+    });
+    const { data } = response;
+    console.log('data', data);
 
-  const response = await axios.get(`${envConfig.API_GITHUB_REPO}`, {
-    headers: {
-      Authorization: `token ${envConfig.GH_TOKEN}`,
-      Accept: 'application/vnd.github.v3+json',
-    },
-    params,
-  });
-  const { data } = response;
+    const repositories = data?.filter((item: any) => item.fork === false);
 
-  const repositories = data?.filter((item: any) => item.fork === false);
+    const projects: IProject[] = await Promise.all(
+      repositories.map(async (repo: any) => {
+        const {
+          owner,
+          name,
+          id,
+          html_url: html,
+          visibility,
+          description,
+          license,
+          topics,
+          forks,
+          stargazers_count: count,
+          created_at: created,
+          updated_at: updated,
+        } = repo;
 
-  const projects: IProject[] = await Promise.all(
-    repositories.map(async (repo: any) => {
-      const {
-        owner,
-        name,
-        id,
-        html_url: html,
-        visibility,
-        description,
-        license,
-        topics,
-        forks,
-        stargazers_count: count,
-        created_at: created,
-        updated_at: updated,
-      } = repo;
-
-      // Promise
-      const contributorsPromise = getContributors(owner.login, name);
-      const trafficClonesPromise = getRepoTrafficClones(owner.login, name);
-      const trafficViewsPromise = getRepoTrafficViews(owner.login, name);
-      const languagesPromise = getRepoLanguages(owner.login, name);
-      // Xu ly promise
-      const [contributors, trafficClones, trafficViews, languages] = await Promise.all([
-        contributorsPromise,
-        trafficClonesPromise,
-        trafficViewsPromise,
-        languagesPromise,
-      ]);
-      return {
-        id,
-        name,
-        visibility,
-        html_url: html,
-        description,
-        license,
-        topics,
-        forks,
-        stargazers_count: count,
-        contributors,
-        trafficClones,
-        trafficViews,
-        languages,
-        created_at: created,
-        updated_at: updated,
-      };
-    }),
-  );
-  // console.log('project service');
-  return projects;
+        // Promise
+        const contributorsPromise = getContributors(owner.login, name);
+        const trafficClonesPromise = getRepoTrafficClones(owner.login, name);
+        const trafficViewsPromise = getRepoTrafficViews(owner.login, name);
+        const languagesPromise = getRepoLanguages(owner.login, name);
+        // Xu ly promise
+        const [contributors, trafficClones, trafficViews, languages] = await Promise.all([
+          contributorsPromise,
+          trafficClonesPromise,
+          trafficViewsPromise,
+          languagesPromise,
+        ]);
+        return {
+          id,
+          name,
+          visibility,
+          html_url: html,
+          description,
+          license,
+          topics,
+          forks,
+          stargazers_count: count,
+          contributors,
+          trafficClones,
+          trafficViews,
+          languages,
+          created_at: created,
+          updated_at: updated,
+        };
+      }),
+    );
+    console.log('project service', projects);
+    return projects || [];
+  } catch (err) {
+    console.log('err service', err);
+    return [];
+  }
 };
